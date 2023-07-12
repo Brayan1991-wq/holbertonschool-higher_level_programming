7b7b55b96927823fbe0a64170791acb1d7df5b32
Brayan1991-wq <brayanmalafaya17@gmail.com>
2023-07-12 15:14:06 -0700

    Program that prints the last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,10 +4,10 @@ number = random.randint(-10000, 10000)
 last = abs(number) % 10
 if number < 0:
     last = -last
-    print("Last digit of {} is {} and is ".format(number, last), end="")
 if last > 5:
-    print("greater than 5")
+    print("Last digit of {} is {} and is greater than 5".format(number, last))
 elif last == 0:
-    print("0")
+    print("Last digit of {} is {} and is 0".format(number, last))
 else:
-    print("less than 6 and not 0")
+    print("Last digit of {} is {} and is less than 6 and not 0"
+          .format(number, last))
