cf48b999d43bacdde8340224d4200e7a8e37ed5e
Brayan1991-wq <brayanmalafaya17@gmail.com>
2023-07-13 14:03:08 -0700

    Print different 2-digit combinations

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for com1 in range(0, 10):
+    for com2 in range(com1 + 1, 10):
+        if com1 == 8 and com2 == 9:
+            print("{}{}".format(com1, com2))
+        else:
+            print("{}{}".format(com1, com2), end= ", ")
