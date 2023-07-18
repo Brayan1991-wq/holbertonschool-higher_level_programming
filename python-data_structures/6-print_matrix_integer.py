3ce8077a2a0670de8aa426f85f8c4ae843790ce2
Brayan1991-wq <brayanmalafaya17@gmail.com>
2023-07-18 15:09:25 -0700

    function that prints a matrix of integers

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matriz=[[]]):
+    for i, fila in enumerate(matriz):
+        for j, elemento in enumerate(fila):
+            print("{:d}". format(elemento), end=" ")
+            if (j + 1) % 3 == 0:
+                print()
