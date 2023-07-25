9b34c9c8546b1510a7cda7450e0748b801a8fefb
Brayan1991-wq <brayanmalafaya17@gmail.com>
2023-07-25 16:51:11 -0700

    class Square that defines a square by: (based on 5-square.py)

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -4,16 +4,33 @@
 
 class Square:
     def __init__(self, size=0, position=(0, 0)):
+        """initializes the object's attributes"""
 
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """
+        Getter para obtener el tamaño del cuadrado.
+
+        Retorna:
+            int: Tamaño del cuadrado.
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Setter para establecer el tamaño del cuadrado.
+
+        Parámetros:
+            value (int): Tamaño del cuadrado.
+
+        Lanza:
+            TypeError: Si el valor no es un número entero.
+            ValueError: Si el valor es negativo.
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -22,9 +39,24 @@ class Square:
 
     @property
     def position(self):
+        """
+        Getter para obtener la posición del cuadrado en la impresión.
+
+        Retorna:
+            tuple: Posición del cuadrado en la impresión (x, y).
+        """
         return self.__podition
 
     @position.setter
+    """
+        Setter para establecer la posición del cuadrado en la impresión.
+
+        Parámetros:
+            value (tuple): Posición del cuadrado en la impresión (x, y).
+
+        Lanza:
+            TypeError: Si el valor no es una tupla de 2 enteros positivos.
+        """
     def position(self, value):
         if not isinstance(value, tuple) or len(value) != 2 or \
            not all(isinstance(i, int) and i >= 0 for i in value):
@@ -32,9 +64,22 @@ class Square:
         self.__position = value
 
     def area(self):
+        """
+        Calcula el área del cuadrado.
+
+        Retorna:
+            int: Área del cuadrado (lado * lado).
+        """
         return self.__size ** 2
 
     def my_print(self):
+        """
+        Imprime el cuadrado en la salida estándar (stdout).
+
+        Si el tamaño es 0, imprime una línea vacía.
+        La posición se usa para
+        agregar espacios a la izquierda cuando la posición[1] > 0.
+        """
         if self.__size == 0:
             print()
         else:
