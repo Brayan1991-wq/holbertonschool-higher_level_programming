43753d244473ba1f4f2f457084d8d31d9bca3507
Brayan1991-wq <brayanmalafaya17@gmail.com>
2023-08-30 13:27:09 -0700

    script that creates the MySQL server user user_0d_1.

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- cree el usuario del servidor MySQL user_0d_1.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
